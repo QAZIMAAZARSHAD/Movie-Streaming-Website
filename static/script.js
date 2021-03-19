dd867400db3a50100e4af37d50f8dc5834c7bbc9
qazimaazarshad <qazimaazarshad@gmail.com>
2021-03-19 14:53:39 +0530

    Add

---
+++ static/script.js
@@ -0,0 +1,14 @@
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0]; 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
