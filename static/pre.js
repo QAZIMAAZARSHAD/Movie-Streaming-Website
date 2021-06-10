2a1b6f9b6ff035070dcbd5841fae632dca8a40d9
Shiv <69760883+shivshankar9@users.noreply.github.com>
2021-06-10 21:19:09 +0530

    Update pre.js
    Added ja code

--- static/pre.js
+++ static/pre.js
@@ -1,1 +1,28 @@
+var select = function(s) {
+    return document.querySelector(s);
+  },
+  selectAll = function(s) {
+    return document.querySelectorAll(s);
+  }, 
+  animationWindow = select('#animationWindow'),    
+    animData = {
+		wrapper: animationWindow,
+		animType: 'svg',
+		loop: true,
+		prerender: true,
+		autoplay: true,
+		path: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/35984/play_fill_loader.json',
+  rendererSettings: {
+    //context: canvasContext, // the canvas context
+    //scaleMode: 'noScale',
+    //clearCanvas: false,
+    //progressiveLoad: false, // Boolean, only svg renderer, loads dom elements when needed. Might speed up initialization for large number of elements.
+    //hideOnTransparent: true //Boolean, only svg renderer, hides elements when opacity reaches 0 (defaults to true)
+  }   
+	}, anim;
 
+	anim = bodymovin.loadAnimation(animData);
+ anim.addEventListener('DOMLoaded', onDOMLoaded);
+ anim.setSpeed(1);
+
+function onDOMLoaded(e){
