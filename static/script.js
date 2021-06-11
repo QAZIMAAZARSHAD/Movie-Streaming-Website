33241d1ccb3d08171156b5e16f5021f3af9494f5
Harshita <goswamiharshita0802@gmail.com>
2021-06-11 14:45:07 +0530

    added dark/light mode

--- static/script.js
+++ static/script.js
@@ -1,10 +1,10 @@
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0]; 
-btn.onclick = function() {
+window.onclick = function() {
   modal.style.display = "block";
 }
-span.onclick = function() {
+window.onclick = function() {
   modal.style.display = "none";
 }
 window.onclick = function(event) {
@@ -12,3 +12,12 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+const ball = document.querySelector(".toggle-container");
+const items = document.querySelectorAll("body,.menu,.menu ul li a,.toggle,button,marquee,.logo,.menu2,.menu2 ul li a,.image-holder,.container,.flim,.hr,.maincontainer,.foot,.close,.close:hover,.close:focus,.modal,.modal-content,.buttonmodal button,.online");
+ball.addEventListener("click",() =>{
+  items.forEach(item =>{
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
