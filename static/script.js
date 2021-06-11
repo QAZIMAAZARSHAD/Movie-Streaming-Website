2ba7d6dcd856b82d021b42aad73d2c5c08b57091
Aum Singhal <pycoder77@gmail.com>
2021-06-11 09:41:46 +0530

    Issue #10 Add play and download button when hovering on movie image
    I have created play and download button which will appear on hover.
    
    Also made the code much more easier to read by adding comments, indents and spaces cause it was becoming difficult to work on this kind of large project 😅 Hope It is fine.
    
    Please let me know if you need any changes to be done

--- static/script.js
+++ static/script.js
@@ -1,14 +1,32 @@
+
+// Variables intialisation
+
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0]; 
+
+
+// Functions
+
 btn.onclick = function() {
   modal.style.display = "block";
 }
+
 span.onclick = function() {
   modal.style.display = "none";
 }
+
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+function myFunction() {
+  var x = document.getElementById("Home");
+  if (x.className === "menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "menu";
   }
 }
