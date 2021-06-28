483bae25916d57581f0ac8d2d8e8973029cc252b
Akshit42-hue <patelakshit2025@gmail.com>
2021-06-28 18:08:59 +0530

    google authentication

---
+++ script.js
@@ -0,0 +1,18 @@
+    function onSignIn(googleUser) {
+      // Useful data for your client-side scripts:
+      document.location.href = 'http://localhost/dashboard/Movie-Streaming-Website/home.html';
+
+
+
+    
+      
+      //window.location.href = "http://localhost/dashboard/Movie-Streaming-Website/home.html";
+      // The ID token you need to pass to your backend:
+
+          // The user is now signed in on the server too
+          // and the user should now have a session cookie
+          // for the whole site. 
+     
+      
+      
+    };
